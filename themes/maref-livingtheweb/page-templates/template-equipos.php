d678fb01278d2394b6c3313071c83c8735cfd945
Mariano <livingtheweb01@gmail.com>
2020-07-25 20:05:30 -0300

    Página equipos pero faltan detalles de diseño

---
+++ themes/maref-livingtheweb/page-templates/template-equipos.php
@@ -0,0 +1,152 @@
+<?php
+/**
+ * Template name: Equipos
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Maref_by_Livingtheweb!
+ */
+get_header();
+?>
+
+
+<div id="primary">
+	<main id="main" class="site-main">    
+    <section class="bg-caja-02">
+      <div class="container-fluid">
+          <div class="row align-items-center">           
+            <div class="col-lg-6 py-5">
+              <div class="caja px-5">
+                <h2 class="text-blue font-weight-bold text-uppercase mb-2">Instrumentos de medición</h2>                
+                <p class="text-left text-blue">Brindamos servicios de medición y alquiler de telemedición de sondaje a nivel internacional respaldados por empresas reconocidas mundialmente como DEVICO en Argentina y Uruguay, y Boart Longyear en Chile</p>
+                <a href="" class="btn btn-lg btn-orange">Más información</a>
+              </div>
+            </div>
+            <div class="col-lg-3 text-center py-5">   
+              <img src="<?php echo site_url(); ?>/wp-content/uploads/2020/07/Boart.png" alt="">           
+            </div>  
+            <div class="col-lg-3 bg-caja-02 text-center py-5">    
+              <img src="<?php echo site_url(); ?>/wp-content/uploads/2020/07/logo_devico_small.png" alt="">          
+            </div>                         
+        </div>
+      </div>
+    </section>
+
+    <!-- Corregir clase section-servicios, que sea general -->
+    <section id="central-right" class="section-p60 bg-general">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8">
+            <h2 class="text-blue font-weight-bold text-uppercase mb-2">Orientación de testigos</h2> 
+            <div class="row">
+              <div class="col-lg-6">
+                <h3 class="text-blue font-weight-bold text-uppercase mb-2">DeviCore</h3>  
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum a sapiente, quibusdam eligendi eum molestiae excepturi perferendis tenetur officiis. Fugiat id, temporibus odit eligendi cum nobis esse aliquam ea a!</p>
+                <img src="<?php echo site_url(); ?>/wp-content/uploads/2020/07/equipos-devicore.jpg" alt="">               
+              </div>
+              <div class="col-lg-6">
+                <h3 class="text-blue font-weight-bold text-uppercase mb-2">TruCore</h3>  
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit eum molestiae vero similique laudantium officia quas sed, rerum asperiores omnis placeat numquam quidem aperiam, optio, fugiat id natus dolorum architecto!</p>
+                <img src="<?php echo site_url(); ?>/wp-content/uploads/2020/07/equipos-trucore.jpg" alt="">  
+              </div>
+            </div> 
+            <h2 class="text-blue font-weight-bold text-uppercase mb-2">Right Aligners / Busca Nortes</h2> 
+            <div class="row">
+              <div class="col-lg-6">
+                <h3 class="text-blue font-weight-bold text-uppercase mb-2">DeviSight</h3>  
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum a sapiente, quibusdam eligendi eum molestiae excepturi perferendis tenetur officiis. Fugiat id, temporibus odit eligendi cum nobis esse aliquam ea a!</p>
+                <img src="<?php echo site_url(); ?>/wp-content/uploads/2020/07/equipos-devisight.jpg" alt="">               
+              </div>
+              <div class="col-lg-6">
+                <h3 class="text-blue font-weight-bold text-uppercase mb-2">DeviAligner</h3>  
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit eum molestiae vero similique laudantium officia quas sed, rerum asperiores omnis placeat numquam quidem aperiam, optio, fugiat id natus dolorum architecto!</p>
+                <img src="<?php echo site_url(); ?>/wp-content/uploads/2020/07/equipos-devialigner.jpg" alt="">  
+              </div>
+            </div>  
+          </div> <!-- /.col-lg-8 -->
+          <div class="col-lg-3">              
+            <h2 class="text-blue font-weight-bold text-uppercase mb-2">Medición de trayectoria</h2>
+            <article>
+              <h3 class="text-blue font-weight-bold text-uppercase mb-2">DeiShot</h3>  
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit eum molestiae vero similique laudantium officia quas sed, rerum asperiores omnis placeat numquam quidem aperiam, optio, fugiat id natus dolorum architecto!</p>
+              <img src="<?php echo site_url(); ?>/wp-content/uploads/2020/07/equipos-devishot.jpg" alt="">    
+            </article>   
+            <article>
+              <h3 class="text-blue font-weight-bold text-uppercase mb-2">DeviGyro</h3>  
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit eum molestiae vero similique laudantium officia quas sed, rerum asperiores omnis placeat numquam quidem aperiam, optio, fugiat id natus dolorum architecto!</p>
+              <img src="<?php echo site_url(); ?>/wp-content/uploads/2020/07/equipos-devigyro.jpg" alt="">    
+            </article>   
+            <article>
+              <h3 class="text-blue font-weight-bold text-uppercase mb-2">TruShot</h3>  
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit eum molestiae vero similique laudantium officia quas sed, rerum asperiores omnis placeat numquam quidem aperiam, optio, fugiat id natus dolorum architecto!</p>
+              <img src="<?php echo site_url(); ?>/wp-content/uploads/2020/07/equipos-trushot.jpg" alt="">    
+            </article>           
+          </div>
+        </div>
+      </div>     
+    </section> 
+
+    <aside class="py-5 bg-btn-app">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8 offset-lg-2">
+            <?php get_search_form(); ?>
+          </div>
+        </div>
+      </div>
+    </aside>
+  
+    <section id="control-solidos" class="section-p60 bg-general">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <h2 class="font-weight-bold text-blue text-center display-4 mb-5">Sistema de control de sólidos</h2>         
+          </div>
+          <div class="mx-4"></div>
+          <div class="row">
+            <div class="col-lg-5 text-left">              
+              <h3 class="text-blue font-weight-bold text-uppercase mb-5">Equipos para perforación de dinammita</h3>  
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia fugit, illum, repellendus impedit veritatis quidem ut commodi pariatur at earum odit recusandae quos, perferendis dolore molestiae similique nisi harum fuga.</p>    
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam commodi quasi ipsam sit ullam saepe ab vel accusamus perferendis ducimus totam molestiae recusandae, enim qui quam corporis quae sunt dolorem?</p>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem accusantium delectus laborum? Consequatur molestias, porro sapiente ducimus qui veritatis eius. Rem similique, et cumque dolor necessitatibus reprehenderit tenetur assumenda non?</p>
+              <a href="" class="btn btn-orange text-uppercase">Más información</a>               
+            </div>
+            <div class="col-lg-7">    
+              <div class="row">
+                <div class="col-lg-12">                   
+                  <img src="<?php echo site_url(); ?>/wp-content/uploads/2020/07/ucs-1.jpg" alt="">  
+                  <hr>
+                </div>
+                
+                <div class="col-lg-8">
+                  <img src="<?php echo site_url(); ?>/wp-content/uploads/2020/07/ucs-2.jpg" alt="">  
+                </div>
+                <div class="col-lg-4" style="background-image:url(<?php echo site_url(); ?>/wp-content/uploads/2020/07/ucs-3.jpg); background-repeat:no-repeat;background-position:center center; background-size:cover"></div>
+              
+            </div>
+          </div>
+        </div>
+      </div>     
+    </section> 
+    <section id="equipos-rotacion" class="section-p60 bg-general">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-8">
+            <img src="https://via.placeholder.com/900x450" alt="">
+          </div>
+          <div class="col-lg-4">
+            <h3 class="text-blue font-weight-bold text-uppercase mb-5">Equipos para perforación de dinammita</h3> 
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias quam hic itaque culpa ab voluptatum, suscipit vel alias possimus necessitatibus aspernatur, laudantium, veritatis ratione quisquam aut nulla inventore perspiciatis iste? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti quas velit, quidem voluptas optio, unde minima reprehenderit aliquid ea asperiores odio ducimus magnam? Ipsa eveniet culpa, autem accusamus optio eaque?</p>
+            <a href="" class="btn btn-orange text-uppercase">Más información</a>   
+          </div>
+        </div>
+      </div>
+    </section>
+
+	</main><!-- #main -->
+</div>
+
+<?php
+get_footer();
+  
+
